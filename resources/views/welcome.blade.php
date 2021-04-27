c091394cdff572d651f8adf04d8c38d187076872
”Yurii <yura.kornssnko@gmail.com>
2021-04-27 19:58:53 +0300

    Mixed content problem was fixed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,8 +6,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="css/fonts.css">
-  <link rel="stylesheet" href="css/app.css">
+  <link rel="stylesheet" href="{{ asset('css/fonts.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 
 <body id="home">
@@ -316,7 +316,7 @@
     </section>
   @endisset
 
-  <script src="js/app.js"></script>
+  <script src="{{ asset('js/app.js') }}"></script>
 </body>
 
 </html>
